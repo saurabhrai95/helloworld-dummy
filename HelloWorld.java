ff733a7429c7104ee032e876c3a4833a25644fa4
saurabhrai95 <32508601+saurabhrai95@users.noreply.github.com>
2018-08-23 18:31:52 +0530

    syso updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!! Edited");
+		System.out.println("Hello World!! Finally jenkins is up and running");
 	}
 }
